f993ae6910a349013d7bfb3bd5f353710ef9476b
hashim-alqady <hashimalqady5@gmail.com>
2023-08-01 13:25:20 +0300

    first commit

---
+++ vpc.tf
@@ -0,0 +1,22 @@
+#resource "aws_vpc" "my_vpc" {
+ # cidr_block = "10.0.0.0/16"
+  #enable_dns_support = true
+  #enable_dns_hostnames = true
+
+  #tags = {
+   # Name = "test"
+  #}
+  #}
+
+  resource "aws_instance" "my-instance" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+  #key_name      = "your_key_pair_name"
+  #subnet_id     = "your_subnet_id"
+  security_groups = ["${aws_security_group.upuntusg.name}"]
+
+  tags = {
+    Name = "my-instance"
+  }
+}
+
